3787e979c12792aba38d5e4eeb53984a61b1e437
Jakub Ciemięga <jakub.ciemiega@gmail.com>
2022-04-22 15:16:47 +0200

    loading screen changes

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../home.dart';
-import '../info.dart';
-import '../plan_page.dart';
-import '../players_page.dart';
-import '../teams_page.dart';
-
 class Header extends StatelessWidget {
   const Header(this.heading);
   final String heading;
@@ -78,54 +72,3 @@ Widget loadingIndicator = new Container(
         child: new Center(child: new CircularProgressIndicator())),
   ),
 );
-
-
-class MyNavigationBar extends StatefulWidget {
-  const MyNavigationBar({Key? key}) : super(key: key);
-
-  @override
-  State<MyNavigationBar> createState() => _MyNavigationBarState();
-}
-
-class _MyNavigationBarState extends State<MyNavigationBar> {
-  int _selectedIndex = 0;
-  final List<Widget> _children = [
-    HomeScreen(),
-    PlanPage(),
-    PlayersPage(),
-    TeamsPage(),
-    ReadScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _children[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.blueGrey[100],
-        unselectedItemColor: Colors.blueGrey,
-        selectedItemColor: Colors.blue,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month), label: 'Plan'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person), label: 'Zawodnicy'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.people), label: 'Drużyny'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.newspaper), label: 'Czytelnia'),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
